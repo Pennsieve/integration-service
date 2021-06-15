7a2c6fff161e363819313ae03a40e0182d51e375
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2021-06-15 11:54:59 -0400

    updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@
 # PLATFORM EVENTS SNS INTEGRATION            #
 ##############################################
 
-output "ncs_integration_events_sns_topic_arn" {
+output "integration_events_sns_topic_arn" {
   value = aws_sns_topic.integration_events_sns_topic.arn
 }
