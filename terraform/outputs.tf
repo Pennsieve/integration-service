f33dcf5ae8f3be5b3d52765e5f82346ccbf979c1
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2021-06-29 12:35:44 -0400

    added name output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@
 output "integration_events_sns_topic_arn" {
   value = aws_sns_topic.integration_events_sns_topic.arn
 }
+
+output "integration_events_sns_topic_name" {
+  value = aws_sns_topic.integration_events_sns_topic.name
+}
