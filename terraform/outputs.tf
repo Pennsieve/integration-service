f39bcef6ef80fc5411c38ac681f866ce5586190a
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2021-06-30 11:48:58 -0400

    add kms arn

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,7 @@ output "integration_events_sns_topic_arn" {
 output "integration_events_sns_topic_name" {
   value = aws_sns_topic.integration_events_sns_topic.name
 }
+
+output "integration_events_kms_key_arn" {
+  value = aws_kms_key.event_integration_sqs_kms_key.arn
+}
