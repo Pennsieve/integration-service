0b5abe8104962f97c4b6867272dca56061b71fc5
Edmore Moyo <etmoyo@gmail.com>
2023-07-21 14:40:30 -0400

    includes lambda rie in image

--- lambda/service/models/integration.go
+++ lambda/service/models/integration.go
@@ -5,7 +5,6 @@ type Integration struct {
 	DatasetID      string         `json:"datasetId"`
 	ApplicationID  int64          `json:"applicationId"`
 	TriggerPayload TriggerPayload `json:"payload"`
-	TriggerClient  string         `json:"client"`
 }
 
 type TriggerPayload struct {
