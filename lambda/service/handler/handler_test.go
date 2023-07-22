3b2ef9de6c302c93dea5e3fbe4bfcb2afd31c348
Edmore Moyo <etmoyo@gmail.com>
2023-07-21 21:34:52 -0400

    fixes typo

--- lambda/service/handler/handler_test.go
+++ lambda/service/handler/handler_test.go
@@ -17,7 +17,7 @@ func TestIntegrationServiceHandler(t *testing.T) {
 	}
 	request := events.APIGatewayV2HTTPRequest{
 		RouteKey:       "POST /IncorrectIntegrationsRoute",
-		Body:           "{ \"sessionToken\": \"ae5t678999-a345fgg\", \"datasetI\": \"dataset123\", \"applicationId\": 1, \"payload\": {\"packageIds\": [1,2,3]}}",
+		Body:           "{ \"sessionToken\": \"ae5t678999-a345fgg\", \"datasetId\": \"dataset123\", \"applicationId\": 1, \"payload\": {\"packageIds\": [1,2,3]}}",
 		RequestContext: requestContext,
 	}
 
