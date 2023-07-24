85ed2dc58c88e2a66ea7b120365fd4bd04e82574
Edmore Moyo <etmoyo@gmail.com>
2023-07-24 16:48:24 -0400

    updates tf files

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ LAMBDA_BUCKET ?= "pennsieve-cc-lambda-functions-use1"
 WORKING_DIR   ?= "$(shell pwd)"
 API_DIR ?= "api"
 SERVICE_NAME  ?= "integration-service"
+SERVICE_PACKAGE_NAME ?= "integration-service-${IMAGE_TAG}.zip"
 PACKAGE_NAME  ?= "${SERVICE_NAME}-${IMAGE_TAG}.zip"
 
 .DEFAULT: help
@@ -39,13 +40,22 @@ docker-clean:
 package:
 	@echo ""
 	@echo "***********************"
-	@echo "*   Packaging lambda   *"
+	@echo "*   Packaging Python lambda   *"
 	@echo "***********************"
 	@echo ""
 	cd $(WORKING_DIR)/lambda/ ; \
 		mkdir bin; \
 		cd event_lambda/; \
 			zip -r $(WORKING_DIR)/lambda/bin/$(PACKAGE_NAME) .
+	@echo ""
+	@echo "***********************"
+	@echo "*   Building integration Service lambda   *"
+	@echo "***********************"
+	@echo ""
+	cd $(WORKING_DIR)/lambda/service; \
+  		env GOOS=linux GOARCH=amd64 go build -o $(WORKING_DIR)/lambda/bin/service/pennsieve_integration_service; \
+		cd $(WORKING_DIR)/lambda/bin/service/ ; \
+			zip -r $(WORKING_DIR)/lambda/bin/service/$(SERVICE_PACKAGE_NAME) .
 
 # Copy Service lambda to S3 location
 publish:
@@ -55,5 +65,12 @@ publish:
 	@echo "*   Publishing lambda   *"
 	@echo "*************************"
 	@echo ""
-	aws s3 cp $(WORKING_DIR)/lambda/bin/$(PACKAGE_NAME) s3://$(LAMBDA_BUCKET)/$(SERVICE_NAME)/
+	aws s3 cp $(WORKING_DIR)/lambda/bin/$(PACKAGE_NAME) s3://$(LAMBDA_BUCKET)/$(SERVICE_NAME)/event_handler/
 	rm -rf $(WORKING_DIR)/lambda/bin/$(PACKAGE_NAME)
+	@echo ""
+	@echo "*************************"
+	@echo "*   Publishing Service lambda   *"
+	@echo "*************************"
+	@echo ""
+	aws s3 cp $(WORKING_DIR)/lambda/bin/service/$(SERVICE_PACKAGE_NAME) s3://$(LAMBDA_BUCKET)/$(SERVICE_NAME)/service/
+	rm -rf $(WORKING_DIR)/lambda/bin/service/$(SERVICE_PACKAGE_NAME)
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "event_integration_consumer_lambda" {
   timeout          = 60
   memory_size      = 128
   s3_bucket         = var.lambda_bucket
-  s3_key            = "${var.service_name}/${var.service_name}-${var.image_tag}.zip"
+  s3_key            = "${var.service_name}/event_handler/${var.service_name}-${var.image_tag}.zip"
 
   vpc_config {
     subnet_ids         = tolist(data.terraform_remote_state.vpc.outputs.private_subnet_ids)
@@ -21,6 +21,31 @@ resource "aws_lambda_function" "event_integration_consumer_lambda" {
       ENV = var.environment_name
       PENNSIEVE_DOMAIN = data.terraform_remote_state.account.outputs.domain_name,
 #      WEBHOOK_SQS_QUEUE_NAME = aws_sqs_queue.webhook_integration_queue.name
+    }
+  }
+}
+
+## Lambda Function for Integration Service.
+resource "aws_lambda_function" "integration_service_lambda" {
+  description      = "Lambda Function which provides the interface for integration applications"
+  function_name    = "${var.environment_name}-${var.service_name}-lambda-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
+  handler          = "integration-service"
+  runtime          = "provided.al2"
+  role             = aws_iam_role.integration_service_lambda_role.arn
+  timeout          = 30
+  memory_size      = 128
+  s3_bucket         = var.lambda_bucket
+  s3_key            = "${var.service_name}/service/${var.service_name}-${var.image_tag}.zip"
+
+  vpc_config {
+    subnet_ids         = tolist(data.terraform_remote_state.vpc.outputs.private_subnet_ids)
+    security_group_ids = [data.terraform_remote_state.platform_infrastructure.outputs.integration_service_security_group_id]
+  }
+
+  environment {
+    variables = {
+      ENV = var.environment_name
+      PENNSIEVE_DOMAIN = data.terraform_remote_state.account.outputs.domain_name,
     }
   }
 }
