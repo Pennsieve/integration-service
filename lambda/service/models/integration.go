ee24a44c970c86112da00463a19f05e896cdca4c
Edmore Moyo <etmoyo@gmail.com>
2023-08-30 08:53:53 -0400

    adds new model

--- lambda/service/models/integration.go
+++ lambda/service/models/integration.go
@@ -9,6 +9,11 @@ type Integration struct {
 }
 
 type TriggerPayload struct {
-	PackageIDs    []int64  `json:"packageIds"`
-	PresignedURLs []string `json:"presignedURLs"`
+	PackageIDs    []int64 `json:"packageIds"`
+	PresignedURLs []Files `json:"presignedURLs"`
+}
+
+type Files struct {
+	Filename string `json:"filename"`
+	URL      string `json:"url"`
 }
