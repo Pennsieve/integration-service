9a33c316a61fa8ca76ea2e95cf6e979574558be8
Edmore Moyo <etmoyo@gmail.com>
2024-05-27 17:57:39 -0400

    fix for GET endpoint

--- lambda/service/models/integration.go
+++ lambda/service/models/integration.go
@@ -1,22 +1,15 @@
 package models
 
 type Integration struct {
-	Uuid          string        `json:"uuid"`
-	ApplicationID int64         `json:"applicationId"`
-	ComputeNode   ComputeNode   `json:"computeNode"`
-	DatasetNodeID string        `json:"datasetId"`
-	PackageIDs    []string      `json:"packageIds"`
-	Workflow      []Application `json:"workflow"`
-	Params        interface{}   `json:"params,omitempty"`
+	Uuid          string      `json:"uuid"`
+	ApplicationID int64       `json:"applicationId"`
+	ComputeNode   ComputeNode `json:"computeNode"`
+	DatasetNodeID string      `json:"datasetId"`
+	PackageIDs    []string    `json:"packageIds"`
+	Workflow      interface{} `json:"workflow"`
+	Params        interface{} `json:"params,omitempty"`
 }
 
-type Application struct {
-	ApplicationUuid          string      `json:"uuid"`
-	ApplicationId            string      `json:"applicationId"`
-	ApplicationType          string      `json:"applicationType"`
-	ApplicationContainerName string      `json:"applicationContainerName"`
-	Params                   interface{} `json:"params,omitempty"`
-}
 type ComputeNode struct {
 	ComputeNodeUuid       string `json:"uuid"`
 	ComputeNodeGatewayUrl string `json:"computeNodeGatewayUrl"`
