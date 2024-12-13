3381974938728346c1bcb85dbb5f7a53c714e465
Edmore Moyo <etmoyo@gmail.com>
2024-12-13 16:27:36 -0500

    cleans up tests

--- lambda/service/log_retriever/log_retriever_test.go
+++ lambda/service/log_retriever/log_retriever_test.go
@@ -3,23 +3,20 @@ package log_retriever_test
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"testing"
 
-	"github.com/pennsieve/integration-service/service/clients"
 	"github.com/pennsieve/integration-service/service/log_retriever"
+	"github.com/pennsieve/integration-service/service/mocks"
 )
 
 func TestRun(t *testing.T) {
-	// mockClient := mocks.NewMockClient()
-	httpClient := clients.NewComputeRestClient(&http.Client{}, "https://mz23hnbrxrhutrwm2ay7j5ue4u0lbfqe.lambda-url.us-east-1.on.aws/logs")
-	retriever := log_retriever.NewLogRetriever(httpClient,
-		"d25c7b47-de21-4009-a311-bdf950d05a50",
-		"801a4665-ba47-4053-9071-8503efb063ca")
+	mockClient := mocks.NewMockClient()
+	retriever := log_retriever.NewLogRetriever(mockClient,
+		"someWorkflowInstanceId",
+		"someApplicationUUid")
 	ctx := context.Background()
-	resp, err := retriever.Run(ctx)
+	_, err := retriever.Run(ctx)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(resp))
 }
