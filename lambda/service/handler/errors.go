825080bd0f4791640508d9fb9b5faf06f286a7a4
Edmore Moyo <etmoyo@gmail.com>
2024-12-16 11:05:18 -0500

    updates error message and adds validation

--- lambda/service/handler/errors.go
+++ lambda/service/handler/errors.go
@@ -20,6 +20,7 @@ var ErrConfig = errors.New("error loading AWS config")
 var ErrMarshaling = errors.New("error marshaling item")
 var ErrDynamoDB = errors.New("error performing action on DynamoDB table")
 var ErrMissingParameter = errors.New("missing parameter")
+var ErrRunningLogRetriever = errors.New("error running log retriever")
 
 func handlerError(handlerName string, errorMessage error) string {
 	log.Printf("%s: %s", handlerName, errorMessage.Error())
