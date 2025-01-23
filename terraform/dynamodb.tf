77b08f181d9bac2c39faedffa0afdb4f8cae9f62
Rohan Shah <rohanmalavshah@gmail.com>
2025-01-23 13:33:21 -0500

    fix attribute name in dynamodb table

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -51,11 +51,11 @@ tags = merge(
 resource "aws_dynamodb_table" "workflow_instance_status_table" {
   name         = "${var.environment_name}-workflow-instance-status-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "uuid"
+  hash_key     = "workflowInstanceUuid"
   range_key    = "timestamp"
 
   attribute {
-    name = "uuid"
+    name = "workflowInstanceUuid"
     type = "S"
   }
 
